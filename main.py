47d1fdee694ac13dc9ac11b96dda2be82ca7d188
Askar Kasimov <kasimovaskar123@gmail.com>
2024-10-08 18:41:40 +0300

    second task done without tests

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import re
 
 
 def first_task() -> int:
-    input_text = input("Введите текст для подсчёта смайликов типа 8-|\n")
+    input_text = input(
+        "Введите текст для подсчёта смайликов типа 8-|\n").lower()
     return len(re.findall(r"8-\|", input_text))
 
 
+def second_task() -> str:
+    input_text = input("Введите текст для проверки на хайку\n").lower()
+    if len(re.findall(r"/", input_text)) != 2:
+        return "Не хайку. Должно быть 3 строки."
+
+    if re.match(r"^(?:[^аеёиоуыэюя]*[аеёиоуыэюя]){5}[^аеёиоуыэюя]*/(?:[^аеёиоуыэюя]*[аеёиоуыэюя]){7}[^аеёиоуыэюя]*/(?:[^аеёиоуыэюя]*[аеёиоуыэюя]){5}[^аеёиоуыэюя]*$", input_text):
+        return "Хайку!"
+    else:
+        return "Не хайку."
+
+
 if __name__ == "__main__":
-    choosing = input("Выберите номер таска для проверки: 1, 2, 3")
+    choosing = input("Выберите номер таска для проверки: 1, 2, 3\n")
     match choosing:
         case "1":
-            first_task()
+            print("Ответ:", first_task())
         case "2":
-            pass
+            print("Ответ:", second_task())
         case "3":
             pass
         case _:
