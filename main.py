e93ced5a0dd1a2ed6b62d6f0f89cbdd98ddbfa72
Askar Kasimov <kasimovaskar123@gmail.com>
2024-10-09 19:32:54 +0300

    task done without tests

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def second_task() -> str:
         return "Не хайку."
 
 
+def third_task() -> bool:
+    input_text = input("Введите текст для проверки cron-выражения:\n")
+    if re.fullmatch(r"^((([1-5][0-9]|\d|\*)((,([1-5][0-9]|\d))*|-([1-5][0-9]|\d)))|(\*\/([1-5][0-9]|\d))) (((1\d|2[0-3]|\d|\*)((,(1\d|2[0-3]|\d))*|-(1\d|2[0-3]|\d|\*)))|(\*\/(1\d|2[0-3]|\d))) ((([1-2]\d|3[0-1]|[1-9]|L|\*)W?((,([1-2]\d|3[0-1]|[1-9]|L)W?)*|-([1-2]\d|3[0-1]|[1-9]|L)W?))|(\*\/([1-2]\d|3[0-1]|[1-9]))) (((1[0-2]|\d|\?|\*|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)((,(1[0-2]|\d|\*|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec))*|-(1[0-2]|\d|\*|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)))|(\*\/(1[0-2]|\d))) ((([0-6]|\?|\*|L|sun|mon|tue|wed|thu|fri|sat|sun|[0-6]\#[1-5]|\*)((,([0-6]|L|sun|mon|tue|wed|thu|fri|sat|sun|[0-6]\#[1-5]))*|-([0-6]|L|sun|mon|tue|wed|thu|fri|sat|sun|[0-6]\#[1-5])))|(\*\/([0-6]|L|sun|mon|tue|wed|thu|fri|sat|sun|[0-6]\#[1-5])))$", input_text):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     choosing = input("Выберите номер таска для проверки: 1, 2, 3\n")
     match choosing:
@@ -26,6 +34,6 @@ if __name__ == "__main__":
         case "2":
             print("Ответ:", second_task())
         case "3":
-            pass
+            print("Ответ:", third_task())
         case _:
             print("Нет такого таска :(")
